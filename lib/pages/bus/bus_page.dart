f084f1e2b5f097ee0695e016ca65ad4d7eca4e6a
Croissanttttt <psh68892393@gmail.com>
2021-07-11 02:11:01 +0900

    carousel 위젯 파일 분리

--- lib/pages/bus/bus_page.dart
+++ lib/pages/bus/bus_page.dart
@@ -1,172 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:get/get.dart';
+import 'package:getx_app/widgets/carousel.dart';
 
 import 'bus_controller.dart';
 
 class BusPage extends GetView<BusController> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: CarouselWithIndicatorDemo(),
-        ),
-      ),
-    );
-  }
-}
-
-class ManuallyControlledSlider extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _ManuallyControlledSliderState();
-  }
-}
-
-class _ManuallyControlledSliderState extends State<ManuallyControlledSlider> {
-  final CarouselController _controller = CarouselController();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<int> list = [1, 2, 3, 4, 5];
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          CarouselSlider(
-            items: list
-                .map((item) => Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                      child: Center(child: Text(item.toString())),
-                      color: Colors.grey,
-                    ))
-                .toList(),
-            options: CarouselOptions(
-              enlargeCenterPage: false,
-              enableInfiniteScroll: false,
-            ),
-            carouselController: _controller,
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Container(
-              color: Colors.grey,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  ...Iterable<int>.generate(list.length).map(
-                    (int pageIndex) => Flexible(
-                      child: InkWell(
-                        onTap: () {
-                          _controller.animateToPage(pageIndex);
-                        },
-                        child: Ink(
-                          height: 40,
-                          child: Center(child: Text("$pageIndex")),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class CarouselWithIndicatorDemo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _CarouselWithIndicatorState();
-  }
-}
-
-class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
-  List<int> list = [11, 22, 33, 44];
-  int _current = 0;
-  final CarouselController _controller = CarouselController();
+  final List<String> titleList = ['190번', '셔틀버스', '통근버스', '학교버스'];
+  List<dynamic> testPageList = [test1(),test2(),test3(),test4()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: CarouselSlider(
-              items: list
-                  .map((item) => Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                        child: Center(
-                          child: Text(
-                            item.toString(),
-                          ),
-                        ),
-                        color: Colors.grey,
-                      ))
-                  .toList(),
-              carouselController: _controller,
-              options: CarouselOptions(
-                autoPlay: false,
-                enableInfiniteScroll: false,
-                enlargeCenterPage: false,
-                aspectRatio: 2.0,
-                onPageChanged: (index, reason) {
-                  setState(
-                    () {
-                      _current = index;
-                    },
-                  );
-                },
-              ),
-            ),
-          ),
-          Container(
-            height: 40,
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: Colors.black12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: list.asMap().entries.map(
-                (entry) {
-                  return GestureDetector(
-                    onTap: () => _controller.animateToPage(entry.key),
-                    child: Container(
-                      width: 90,
-                      decoration: BoxDecoration(
-                          color:
-                              (Theme.of(context).brightness == Brightness.dark
-                                      ? Colors.white
-                                      : Colors.blue)
-                                  .withOpacity(_current == entry.key ? 0.8 : 0),
-                          borderRadius: BorderRadius.circular(
-                              _current == entry.key ? 10 : 0)),
-                      child: Center(
-                          child: Text(entry.key == 0
-                              ? '190번'
-                              : entry.key == 1
-                                  ? '셔틀버스'
-                                  : entry.key == 2
-                                      ? '통근버스'
-                                      : '학교버스')),
-                    ),
-                  );
-                },
-              ).toList(),
-            ),
-          ),
-        ],
-      ),
+      body: Carousel(pageList: testPageList, titleList: titleList),
     );
+  }
+}
+
+class test1 extends StatelessWidget {
+  const test1({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red,);
+  }
+}
+class test2 extends StatelessWidget {
+  const test2({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blue,);
+  }
+}
+class test3 extends StatelessWidget {
+  const test3({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.black,);
+  }
+}
+class test4 extends StatelessWidget {
+  const test4({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.yellow,);
   }
 }
