69716eeeddf8cbc58184be8ecaed0481bc9be15a
Kim-deokhyeon <kdhmny@naver.com>
2021-07-27 16:57:30 +0900

    fcm 2021 07 27

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
