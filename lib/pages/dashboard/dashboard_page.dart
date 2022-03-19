4d0a9c1c0ec27821b326dec01371f7bbf44be4b8
mukjo96 <mukjo96@gmail.com>
2022-03-19 15:15:17 +0900

    chore : Firebase 관련 라이브러리 제거

--- lib/pages/dashboard/dashboard_page.dart
+++ lib/pages/dashboard/dashboard_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:oceanview/main.dart';
 import 'package:oceanview/pages/home/home_page.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -19,28 +18,10 @@ import 'package:oceanview/pages/more/more_page.dart';
 
 import 'dashboard_controller.dart';
 
-// Crude counter to make messages unique
-// int _messageCount = 0;
-
-/// The API endpoint here accepts a raw FCM payload for demonstration purposes.
-// String constructFCMPayload(String token) {
-//   _messageCount++;
-//   return jsonEncode({
-//     'token': token,
-//     'data': {
-//       'via': 'FlutterFire Cloud Messaging!!!',
-//       'count': _messageCount.toString(),
-//     },
-//     'notification': {
-//       'title': 'Hello FlutterFire!',
-//       'body': 'This notification (#$_messageCount) was created via FCM!',
-//     },
-//   });
-// }
-
 class DashboardPage extends StatefulWidget {
   DashboardPage() {
-    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
 
   @override
@@ -54,110 +35,8 @@ class _DashboardPageState extends State<DashboardPage> {
   void initState() {
     super.initState();
 
-    FirebaseMessaging.instance
-        .getInitialMessage()
-        .then((RemoteMessage? message) {
-      if (message != null) {
-        Get.find<DashboardController>().changeTabIndex(3);
-      }
-    });
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null && !kIsWeb) {
-        flutterLocalNotificationsPlugin!.show(
-            notification.hashCode,
-            notification.title,
-            notification.body,
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                channel!.id,
-                channel!.name,
-                channel!.description,
-                icon: 'launch_background',
-              ),
-            ));
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      print('A new onMessageOpenedApp event was published!');
-      Get.find<DashboardController>().changeTabIndex(3);
-    });
-    // () async {
-    //   print('FlutterFire Messaging Example: Getting APNs token...');
-    //   String? token = await FirebaseMessaging.instance.getAPNSToken();
-    //   print('FlutterFire Messaging Example: Got APNs token: $token');
-    // }();
-    // FirebaseMessaging.instance
-    //     .getToken(
-    //         // vapidKey:
-    //         //     'BGpdLRsMJKvFDD9odfPk92uBg-JbQbyoiZdah0XlUyrjG4SDgUsE1iC_kdRgt4Kn0CO7K3RTswPZt61NNuO0XoA')
-    //         )
-    //     .then((token) {
-    //   print(token);
-    // });
     onRefresh();
   }
-
-  // Future<void> sendPushMessage() async {
-  //   if (_token == null) {
-  //     print('Unable to send FCM message, no token exists.');
-  //     return;
-  //   }
-
-  //   try {
-  //     await http.post(
-  //       Uri.parse('https://api.rnfirebase.io/messaging/send'),
-  //       headers: <String, String>{
-  //         'Content-Type': 'application/json; charset=UTF-8',
-  //       },
-  //       body: constructFCMPayload(_token!),
-  //     );
-  //     print('FCM request for device sent!');
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
-
-  // Future<void> onActionSelected(String value) async {
-  //   switch (value) {
-  //     case 'subscribe':
-  //       {
-  //         print(
-  //             'FlutterFire Messaging Example: Subscribing to topic "fcm_test".');
-  //         await FirebaseMessaging.instance.subscribeToTopic('fcm_test');
-  //         print(
-  //             'FlutterFire Messaging Example: Subscribing to topic "fcm_test" successful.');
-  //       }
-  //       break;
-  //     case 'unsubscribe':
-  //       {
-  //         print(
-  //             'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test".');
-  //         await FirebaseMessaging.instance.unsubscribeFromTopic('fcm_test');
-  //         print(
-  //             'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test" successful.');
-  //       }
-  //       break;
-  //     case 'get_apns_token':
-  //       {
-  //         if (defaultTargetPlatform == TargetPlatform.iOS ||
-  //             defaultTargetPlatform == TargetPlatform.macOS) {
-  //           print('FlutterFire Messaging Example: Getting APNs token...');
-  //           String? token = await FirebaseMessaging.instance.getAPNSToken();
-  //           print('FlutterFire Messaging Example: Got APNs token: $token');
-  //         } else {
-  //           print(
-  //               'FlutterFire Messaging Example: Getting an APNs token is only supported on iOS and macOS platforms.');
-  //         }
-  //       }
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
